bd58e72323781667f4f05efae344457edeee51f8
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-05-13 15:40:56 +0530

    Completed Project 🤩🤩🤩

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-var list = document.getElementById('list')
-var btnclick = document.getElementById('btnclk')
+var list = document.getElementById('todo-list')
+var inputBox = document.getElementById('todo-input')
+var btnclick = document.getElementById('add-item')
 
-var li = document.createElement()
+var currInputValue = ''
+inputBox.addEventListener('input', function(e) {
+    currInputValue = e.target.value;
+});
+
+function toDo() {
+    if (currInputValue !== undefined && currInputValue !== null && currInputValue !== '') {
+        var newElement = document.createElement('li')
+        var textNode = document.createTextNode(currInputValue)
+        newElement.appendChild(textNode)
+        newElement.id = 'item'
+
+        list.appendChild(newElement)
+        console.log(list.childElementCount)
+
+        inputBox.value = ''
+        currInputValue = ''
+
+    } else {
+        alert('Enter a valid list item')
+    }
+}
+
+inputBox.addEventListener('keyup', function(e) {
+    if (e.keyCode === 13) {
+        toDo();
+    }
+})
+
+btnclick.addEventListener('click', toDo);
